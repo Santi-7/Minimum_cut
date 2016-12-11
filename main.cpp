9bb60596826d6f40bae31ebf0e2537cd92fe73fc
mjgalindo <mjgalindoramos@gmail.com>
2016-12-11 21:22:31 +0100

    Check for errors while loading edges from a file

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@
 #include <iostream>
 #include <fstream>
 #include <unordered_map>
+#include <assert.h>
 
 using namespace std;
 
@@ -89,7 +90,11 @@ int main(int argc, char *argv[])
 
         if ((productName1.size() == 0) | (productName2.size() == 0))
             continue;
-
+        if (productMap.find(productName1) == productMap.end() || productMap.find(productName2) == productMap.end())
+        {
+            cout << "Wrong product name, all product names must appear before their connections are defined.\n";
+            return 1;
+        }
         Edge tmpEdge(productMap[productName1].GetPack(), productMap[productName2].GetPack());
         kargerGraph.AddEdge(tmpEdge);
     }

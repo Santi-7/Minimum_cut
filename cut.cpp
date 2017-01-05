eaf37a761eacecc590522ea68aa791953c892991
mjgalindo <mjgalindoramos@gmail.com>
2017-01-05 18:31:47 +0100

    Add documentation and make it more flexible for testing rngs and both karger and karger-steins algorithms

--- cut.cpp
+++ cut.cpp
@@ -1,8 +1,20 @@
+/** ---------------------------------------------------------------------------
+ ** cut.cpp
+ ** An implementation for Karger's and Karger-Stein's algorithm to find the
+ ** minimum cut of a graph. It's ready to try different random number generators
+ ** and see the differences between the results they provide.
+ **
+ ** Author: Miguel Jorge Galindo Ramos, NIA: 679954
+ **         Santiago Gil Begué, NIA: 683482
+ ** -------------------------------------------------------------------------*/
+
 #include <iostream>
 #include <fstream>
 #include <map>
 #include <vector>
 #include <random>
+
+#define RANDOM MERSENNE
 
 using namespace std;
 
@@ -36,6 +48,16 @@ typedef tuple<unsigned int, unsigned int> Edge;
 typedef vector<Edge> EdgeList;
 typedef map<string, unsigned int> ProductList;
 
+/**
+ * Reads a graph from a product file. The file format is:
+ * <Number of products>
+ * <Product name>+ // One each line. Vertex in the graph.
+ * <Product name>|<Product name> // One each line. Edge in the graph.
+ *
+ * @param filename path to the file from which the graph will be loaded.
+ * @return List of the vertices, edges and a map of product name - id for the
+ *  file read.
+ */
 tuple<NodeList, EdgeList, ProductList> readFile(const string& filename)
 {
 	// Open the input file.
@@ -96,15 +118,31 @@ tuple<NodeList, EdgeList, ProductList> readFile(const string& filename)
     return make_tuple(nlist, elist, plist);
 }
 
+/**
+ * @param from Minimum value that can be returned.
+ * @param to Maximum value that can be returned.
+ * @return Random value in the specified range.
+ */
 unsigned int getRandom(unsigned int from, unsigned int to)
 {
+#if RANDOM == MERSENNE
     static random_device rd;     // only used once to initialise (seed) engine
     static mt19937 rng(rd());    // random-number engine used (Mersenne-Twister in this case)
     uniform_int_distribution<int> uni(from, to); // guaranteed unbiased
     return (unsigned int) uni(rng);
+#elif RANDOM == C
+    return rand();
+#endif
 }
 
-/// Karger's algorithm.
+/**
+ * An implementation of Karger's algorithm.
+ * @param nodes Vertices of the graph for which a cut will be returned
+ * @param edges Edges of the graph for which a cut will be returned
+ * @return Possible minimum cut for the input graph. It's not guaranteed to be correct. nodes
+ *  and edges will be updated so that nodes contains only two vectors of vertices and edges only
+ *  the edges remaining in the cut found.
+ */
 unsigned int Karger(NodeList& nodes, EdgeList& edges, unsigned int t = 2)
 {
     while(nodes.size() > t)
@@ -138,8 +176,21 @@ unsigned int Karger(NodeList& nodes, EdgeList& edges, unsigned int t = 2)
     return static_cast<unsigned int>(edges.size());
 }
 
+/**
+ * Karger-Stein's Min-Cut algorithm
+ * @param nodes Vertices of the graph for which a cut will be returned
+ * @param edges Edges of the graph for which a cut will be returned
+ * @return Possible minimum cut for the input graph. It's not guaranteed to be correct. nodes
+ *  and edges will be updated so that nodes contains only two vectors of vertices and edges only
+ *  the edges remaining in the cut found. It's more likely to get the minimum cut than Karger's
+ *  algorithm but also takes longer to run.
+ */
 unsigned int Karger_Stein(NodeList& nodes, EdgeList& edges)
 {
+#if RANDOM == C
+    srand(static_cast<unsigned int>(time(NULL)));
+#endif
+
     if (nodes.size() < 6)
     {
         return Karger(nodes, edges, 2);
@@ -166,22 +217,36 @@ unsigned int Karger_Stein(NodeList& nodes, EdgeList& edges)
     }
 }
 
+/**
+ * Read file from parameters and run Karger's algorithm on the graph described in it.
+ */
 int main(int argc, char * argv[])
 {
     // Check number of parameters.
 	if (argc < 2)
     {
-        cout << "Wrong number of arguments, one needed." << endl;
-        cout << "Usage: " << argv[0] << " <products_file>" << endl;
+        cout << "Wrong number of arguments, at least one needed." << endl;
+        cout << "Usage: " << argv[0] << " <products_file> [-ks]" << endl;
+        cout << "If the option -ks is used then the algorithm run will be Karger-Stein's." << endl;
         return 1;
     }
+    bool useKargerStein = false;
+    if (argc > 2 and string(argv[2]) == "-ks") useKargerStein = true;
 
 	NodeList nodes;
 	EdgeList edges;
 	ProductList products;
 	tie(nodes, edges, products) = readFile(string(argv[1]));
-			
-    unsigned int minimumCut = Karger_Stein(nodes, edges);
+
+    unsigned int minimumCut;
+    if (useKargerStein)
+    {
+        minimumCut = Karger_Stein(nodes, edges);
+    }
+    else
+    {
+        minimumCut = Karger(nodes, edges);
+    }
 
 	for (unsigned int i = 0; i < nodes.size(); ++i)
 	{

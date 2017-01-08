acc33cf9573225a5c1617c50fc9b01c766664ce8
mjgalindo <mjgalindoramos@gmail.com>
2017-01-08 18:57:19 +0100

    Add the possibility to generate weighted graphs

--- graphGen.py
+++ graphGen.py
@@ -4,8 +4,11 @@ import sys
 import random
 
 if len(sys.argv) < 5:
-    print("Usage: ./graphGen.py verticesLeft verticesRight mincut density")
-    exit(1)
+	print("Usage: ./graphGen.py verticesLeft verticesRight mincut density")
+	exit(1)
+addWeight = 0
+if len(sys.argv) == 6:
+	addWeight = int(sys.argv[5])
 
 verticesLeft = int(sys.argv[1])
 verticesRight = int(sys.argv[2])
@@ -16,17 +19,30 @@ print(verticesLeft + verticesRight)
 print(*range(verticesLeft + verticesRight), sep='\n')
 print()
 for vertices in [range(verticesLeft), range(verticesLeft, verticesLeft + verticesRight)]:
-    for vertex in vertices:
-        j = vertex + 1
-        edges = 0
-        while j <= vertices[-1]:
-            if random.random() < density:
-                print(vertex, '|',  j)
-                edges += 1
-            j += 1
-        if edges == 0 and vertex != vertices[-1]:
-            print(vertex, '|', vertex + 1)
-    print()
+	for vertex in vertices:
+		j = vertex + 1
+		edges = 0
+		while j <= vertices[-1]:
+			if random.random() < density:
+				if addWeight != 0:
+					print(vertex, '|',  j, '|', random.randint(1, addWeight))
+				else:
+					print(vertex, '|', j)
+				edges += 1
+			j += 1
+		if edges == 0 and vertex != vertices[-1]:
+			if addWeight != 0:
+				print(vertex, '|', vertex + 1, '|', random.randint(1,addWeight))
+			else:
+				print(vertex, '|', vertex + 1)
+	print()
+if addWeight == 0:
+	for _ in range(mincut):
+		print(random.randint(0,verticesLeft-1), '|', random.randint(verticesLeft, verticesLeft + verticesRight -1))
+else:
+	cutweight = 0
+	while cutweight < mincut:
+		edgeweight = random.randint(1, mincut - cutweight)
+		cutweight += edgeweight
+		print(random.randint(0,verticesLeft-1), '|', random.randint(verticesLeft, verticesLeft + verticesRight -1), '|', edgeweight)
 
-for _ in range(mincut):
-    print(random.randint(0,verticesLeft-1), '|', random.randint(verticesLeft, verticesLeft + verticesRight -1))
